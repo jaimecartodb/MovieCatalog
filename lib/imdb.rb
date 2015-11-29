74a75fd273c5706df7278bd5641de5af2ac2f19a
jaimecartodb <jdemora@cartodb.com>
2015-11-29 12:58:35 -0500

    third iteration

--- lib/imdb.rb
+++ lib/imdb.rb
@@ -3,7 +3,7 @@ require 'imdb'
 class IMDBMovieCatalog
 	attr_reader :search, :movie_titles
 		def search(title)
-		search = Imdb::Seach.new(title)
-		movie_titles= search.movies.map {|movie| movie.title}
+		search = Imdb::Search.new(title)
 	end
 end
+
--- server.rb
+++ server.rb
@@ -1,14 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 require_relative './lib/imdb.rb'
+
+catalog = IMDBMovieCatalog.new
 
 get '/index' do 
 	erb :index
 end
 
-post 'return_movies/search' do
-	catalog = ImdbMovieCatalog.new
-	catalog.search(params[:search])
+post '/return_movies' do
+	@search = catalog.search(params[:theme])
 	erb :show_movies
 end
 
